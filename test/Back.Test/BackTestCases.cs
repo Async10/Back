41109179799266f74a186ffe2d4f9d67d771bf76
Async10 <>
2022-09-27 22:24:53 +0200

    Add first test

---
+++ test/Back.Test/BackTestCases.cs
@@ -0,0 +1,43 @@
+namespace Back.Test;
+
+using System.Diagnostics;
+using Xunit;
+
+public class BackTestCases
+{
+    private const string BackRelativePath = "src/Back/bin/Debug/net6.0/Back.dll";
+    private const string FileRelativPath = "examples/test.back";
+
+    [Fact]
+    public void RunsTestBack()
+    {
+        var process = this.StartCompiler();
+        process.WaitForExit();
+        Assert.Equal(0, process.ExitCode);
+    }
+
+    private Process StartCompiler()
+    {
+        string solutionPath = this.GetSolutionPath();
+        string filePath = Path.Combine(solutionPath, FileRelativPath);
+        string backPath = Path.Combine(solutionPath, BackRelativePath);
+        var process = Process.Start(new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"{backPath} -- {filePath}",
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        });
+        return process ?? throw new ArgumentNullException();
+    }
+
+    private string GetSolutionPath()
+    {
+        return Directory.GetParent(Environment.CurrentDirectory)
+            ?.Parent
+            ?.Parent
+            ?.Parent
+            ?.Parent
+            ?.FullName ?? throw new ArgumentNullException();
+    }
+}
